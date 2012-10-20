f89b21679366b9255d9a4a4405727671cb02d232
Kevin L. Mitchell <klmitch@mit.edu>
2012-10-20 13:12:17 -0500

    Begin toxification of Appathy.

--- setup.py
+++ setup.py
@@ -2,9 +2,17 @@
 
 from setuptools import setup
 
+
+def readreq(filename):
+    with open(filename) as f:
+        reqs = [r.partition('#')[0].strip() for r in f]
+        return [r for r in reqs if r]
+
+
 def readfile(filename):
     with open(filename) as f:
         return f.read()
+
 
 setup(
     name='Appathy',
@@ -16,19 +24,18 @@ setup(
     long_description=readfile('README.rst'),
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
     packages=['appathy'],
-    requires=[
-        "metatools",
-        "routes (>=1.12)",
-        "webob (>=1.1)",
-        "paste.deploy (>=1.5)",
-        "pkg_resources",
-        ],
+    requires=readreq('install-requires'),
+    tests_require=readreq('test-requires'),
     entry_points={
         'appathy.loader': [
             'call = appathy.utils:import_call',
